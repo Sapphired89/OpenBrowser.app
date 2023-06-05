8c2d278c88b0f28cc29d271b8579d9ac508e18aa
U-D3VC0M\helak <helakjack@gmail.com>
2023-06-04 21:40:19 -0400

    Maybe works?

--- src/SapphireBrowser/content.jsx
+++ src/SapphireBrowser/content.jsx
@@ -1,6 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Content = () => {
+  const [openLink, setOpenLink] = useState('');
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const link = event.target.href;
+    setOpenLink(link);
+  };
+
   useEffect(() => {
     const loadGoogleSearchScript = () => {
       const script = document.createElement('script');
@@ -9,21 +17,37 @@ const Content = () => {
       document.body.appendChild(script);
     };
 
-    
-    loadGoogleSearchScript(); 
+    loadGoogleSearchScript();
 
     return () => {
-      const script = document.querySelector('script[src="https://cse.google.com/cse.js?cx=877b9fcfa5ccf4f76"]');
+      const script = document.querySelector(
+        'script[src="https://cse.google.com/cse.js?cx=877b9fcfa5ccf4f76"]'
+      );
       if (script) {
         document.body.removeChild(script);
       }
     };
   }, []);
 
+  useEffect(() => {
+    if (openLink) {
+      // Open link in mini-browser or perform any custom logic
+      console.log('Opening link:', openLink);
+
+      // Reset openLink state after handling the link
+      setOpenLink('');
+    }
+  }, [openLink]);
+
   return (
     <main className="flexContainer">
       <section>
-        <div className="gcse-search"></div>
+        <div className="gcse-search">
+          {/* Add an event listener to the search box */}
+          <a href="https://example.com" onClick={handleClick}>
+            Open Link in Mini-Browser
+          </a>
+        </div>
       </section>
     </main>
   );
