a90dc03b569437ddd790d3d02b9467e9fcceecc4
U-D3VC0M\helak <helakjack@gmail.com>
2023-06-04 22:14:28 -0400

    Loading of web pages

--- src/SapphireBrowser/content.jsx
+++ src/SapphireBrowser/content.jsx
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 const Content = () => {
-  const [openLink, setOpenLink] = useState('');
-
-  const handleClick = (event) => {
-    event.preventDefault();
-    const link = event.target.href;
-    setOpenLink(link);
-  };
-
   useEffect(() => {
     const loadGoogleSearchScript = () => {
       const script = document.createElement('script');
-      script.src = 'https://cse.google.com/cse.js?cx=877b9fcfa5ccf4f76';
+      script.src = 'https://cse.google.com/cse.js?cx=506207fb323e94bf0';
       script.async = true;
       document.body.appendChild(script);
     };
@@ -20,34 +12,17 @@ const Content = () => {
     loadGoogleSearchScript();
 
     return () => {
-      const script = document.querySelector(
-        'script[src="https://cse.google.com/cse.js?cx=877b9fcfa5ccf4f76"]'
-      );
+      const script = document.querySelector('script[src="https://cse.google.com/cse.js?cx=877b9fcfa5ccf4f76"]');
       if (script) {
         document.body.removeChild(script);
       }
     };
   }, []);
 
-  useEffect(() => {
-    if (openLink) {
-      // Open link in mini-browser or perform any custom logic
-      console.log('Opening link:', openLink);
-
-      // Reset openLink state after handling the link
-      setOpenLink('');
-    }
-  }, [openLink]);
-
   return (
     <main className="flexContainer">
       <section>
-        <div className="gcse-search">
-          {/* Add an event listener to the search box */}
-          <a href="https://example.com" onClick={handleClick}>
-            Open Link in Mini-Browser
-          </a>
-        </div>
+        <div className="gcse-search"></div>
       </section>
     </main>
   );
