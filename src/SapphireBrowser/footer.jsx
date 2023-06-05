51fbf15b9003f7d4044cad55053e30c225f6d9ba
U-D3VC0M\helak <helakjack@gmail.com>
2023-06-04 21:08:42 -0400

    Nothing works anymore

--- src/SapphireBrowser/footer.jsx
+++ src/SapphireBrowser/footer.jsx
@@ -3,33 +3,40 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      &copy; 
+      &copy;Sapphire 
       <h5>
         <p>
         User Agreement and Disclaimer:
-
+        </p>
         1.  By using this service, you agree to the following terms and conditions:
         The service provided is for informational purposes only. 
         The content available through this service may not be accurate, complete, or up-to-date. 
         The service should not be considered as professional advice, and you should not rely solely on the information provided.
-
+        <p/>
+        <p>
         2.  The service may include links to third-party websites. These links are provided for convenience and do not signify endorsement or approval of the content on those sites. We have no control over the content or availability of third-party websites and disclaim any responsibility for them.
-
+        </p>
+        <p>
         3.  You acknowledge that any actions you take based on the information provided by this service are at your own risk. 
         We are not liable for any losses, damages, or harm resulting from your use of the service or reliance on its content.
-        
+        </p>
+        <p>
         4.  We make no warranty or representation, express or implied, regarding the accuracy, reliability, or availability of the service. 
         We disclaim all warranties, including but not limited to, warranties of merchantability, fitness for a particular purpose, and non-infringement.
-
+        </p>
+        <p>
         5.  You agree to use the service in compliance with all applicable laws and regulations. 
         You further agree not to use the service for any illegal, unauthorized, or inappropriate activities. 
         We are not responsible for any misuse of the service by users.
-
+        </p>
+        <p>
         6.  We reserve the right to modify, suspend, or terminate the service at any time without notice. 
         We may also update or modify these terms and conditions without prior notice.
-
+        </p>
+        <p>
         7.  This agreement shall be governed by and construed in accordance with the laws of any government, religious, public business, company, or lack there of,  that this program is run on Any disputes arising from or relating to this agreement shall be subject to the exclusive jurisdiction of the courts in [Your Jurisdiction].
-
+        </p>
+        <p>
         By using this service, you acknowledge that you have read, understood, and agreed to the terms and conditions outlined in this agreement.
         </p>
       </h5>
